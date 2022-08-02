6f28528d7e9597ae19110f4501cf16cf3177f93f
JaeyeongSon <nardi0302@gmail.com>
2022-08-02 14:42:03 +0900

    FEAT: Add plugin in postcss
    이미지 fallback을 위한 image-set플러그인 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink1: '#FF7C98',
+        pink2: '#E01883',
+        navy1: '#224289',
+        beige1: '#F9F8F7',
+        brown1: '#918984',
+        brown2: '#726454',
+        white: '#FEFEFE',
+        gray: '#CDCCD5',
+        black1: '#2F231C',
+        black2: '#321C15',
+      },
+    },
   },
   plugins: [],
 };
