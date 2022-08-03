c54dd7a0d78e0e1ec7611befd7cafdd224292eb2
JaeyeongSon <nardi0302@gmail.com>
2022-08-04 01:31:21 +0900

    Merge: merge branch origin main into Menu_page-STYLE-jaeyeong

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
     extend: {
+      screens: {
+        'max-md': { max: '768px' },
+      },
+      fontFamily: {
+        nanum: ['NanumSquareRound', 'sans-serif'],
+        baskin: ['BaskinRobbins', 'sans-serif'],
+      },
+      backgroundImage: (theme) => ({
+        'header-bg': "url('../images/bg_header.gif')",
+        'icon-nav-search': "url('../images/icon_nav_search.png')",
+        'icon-search': "url('../images/icon_search.png')",
+        'icon-facebook': "url('../images/icon_facebook.png')",
+        'icon-twitter': "url('../images/icon_twitter.png')",
+        'icon-instagram': "url('../images/icon_instagram.png')",
+        'icon-blog': "url('../images/icon_blog.png')",
+        'icon-youtube': "url('../images/icon_youtube.png')",
+        'icon-customer-center': "url('../images/icon_customer_center.png')",
+        'btn-happypoint': "url('../images/btn_happypoint.png')",
+        'btn-happymarket': "url('../images/btn_happymarket.png')",
+        'btn-spc': "url('../images/btn_spc_story.png')",
+        'btn-norton': "url('../images/btn_norton.gif')",
+        'btn-ccm': "url('../images/btn_ccm_2.png')",
+        'btn-ksa': "url('../images/btn_ksa.nppng')",
+        'btn-hamburger': "url('../images/btn_hamburger.png')",
+        'btn-close': "url('../images/btn_close.png')",
+        'footer-logo': "url('../images/footer_logo.png')",
+        'select-dropdown': "url('../images/select_dropdown.png')",
+      }),
       colors: {
         pink1: '#FF7C98',
         pink2: '#E01883',
@@ -13,6 +40,7 @@ module.exports = {
         white: '#FEFEFE',
         white1: '#ededed',
         gray: '#CDCCD5',
+        gray2: '#7E7E81',
         black1: '#2F231C',
         black2: '#321C15',
       },
